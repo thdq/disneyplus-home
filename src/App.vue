2d43d421f67968c58291ded0cde48a68ed59f546
Thiago Oliveira <thiagooliv_@outlook.com>
2021-11-06 22:03:43 -0300

    feat: add brand sections to home page

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,24 @@
 
 <template>
   <CarouselTitles />
+  <section class="brands-section">
+    <Brands />
+  </section>
 </template>
 
 <script setup lang="ts">
 import CarouselTitles from './components/CarouselTitles.vue'
+import Brands from './components/Brands.vue'
 </script>
 
-<style>
+<style scoped lang="scss">
+
+.brands-section {
+    margin: 40px 0 40px;
+    display: flex;
+    flex-flow: row wrap;
+    gap: 40px;
+    place-content: center;
+}
+
 </style>
