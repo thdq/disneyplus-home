c3bcae6699ffc36827ecd19563af89806c55a4f3
Thiago Oliveira <thiagooliv_@outlook.com>
2021-11-07 16:36:05 -0300

    fix: adjust props app to render single spa vue app

--- src/main.ts
+++ src/main.ts
@@ -2,21 +2,15 @@ import { createApp, h } from 'vue'
 import App from './App.vue'
 import singleSpaVue, { AppOptions } from 'single-spa-vue'
 
-export let mountParcel: unknown
-
 const vueLifecycles = singleSpaVue({
     createApp,
     appOptions: {
         render() {
             const ctx: AppOptions = this;
-            mountParcel = ctx.mountParcel
             return h(App, {
-                props: {
-
-                    name: ctx.name,
-                    mountParcel: ctx.mountParcel,
-                    singleSpa: ctx.singleSpa,
-                },
+                name: ctx.name,
+                mountParcel: ctx.mountParcel,
+                singleSpa: ctx.singleSpa,
             });
         },
     },
