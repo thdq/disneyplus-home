9af90e863a4f0614b0ec416f327a11a5929a559e
Thiago Oliveira <thiagooliv_@outlook.com>
2021-11-07 00:34:27 -0300

    feat: add Parcel component to home page

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,23 @@
   <section class="brands-section">
     <Brands />
   </section>
+
+  <section class="collection-section">
+    <Parcel 
+      :config="parcelConfig"
+      :mountParcel="mountParcel"
+    />
+  </section>
 </template>
 
 <script setup lang="ts">
 import CarouselTitles from './components/CarouselTitles.vue'
 import Brands from './components/Brands.vue'
+import Parcel from './components/base/Parcel'
+import { mountParcel } from './main'
+
+const parcelConfig = System.import('@disneyplus/disneyplus-collection')
+
 </script>
 
 <style scoped lang="scss">
